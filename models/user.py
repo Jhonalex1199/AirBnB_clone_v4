a116799dd6176134bd77447c76daaff500a25fa5
jhonalex1199 <2050@holbertonschool.com>
2021-05-10 14:48:23 -0500

    models

--- models/user.py
+++ models/user.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 """ holds class User"""
 import models
 from models.base_model import BaseModel, Base
@@ -16,8 +16,9 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
+        places = relationship("Place", backref="user", cascade="all, delete")
+        reviews = relationship("Review", backref="user",
+                               cascade="all, delete")
     else:
         email = ""
         password = ""
